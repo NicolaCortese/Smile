d67956759f5ff796cf5ac033eae9d4712f99f6b1
Nicola Cortese <nico@Nicolas-MacBook-Air.local>
2022-06-23 17:03:44 +0100

    commented out the app test because wtf is it

--- src/test/java/com/example/Smile/SmileApplicationTests.java
+++ src/test/java/com/example/Smile/SmileApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.Smile;
+// package com.example.Smile;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SmileApplicationTests {
+// @SpringBootTest
+// class SmileApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
